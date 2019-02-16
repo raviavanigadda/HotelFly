8ae0a67ccc4ca6ba60690032430519ffcd17fa5b
1895126 <1895126@E21031.CEGEPGIM.LCL>
2019-02-16 12:02:13 -0500

    complete defination

---
+++ Hotelfly/src/hotelfly/Customer.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelfly;
+
+/**
+ *
+ * @author 1895126
+ */
+public class Customer {
+    String firstName,lastName,email;
+    int phoneNo;
+    
+}
