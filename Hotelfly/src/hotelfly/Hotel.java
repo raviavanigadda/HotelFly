92c9af5942adaa7f3d37335eade0e202e4025da0
1894831 <1894831@E21034.CEGEPGIM.LCL>
2019-02-16 11:40:57 -0500

    committing hote

---
+++ Hotelfly/src/hotelfly/Hotel.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelfly;
+
+/**
+ *
+ * @author 1894831
+ */
+public class Hotel {
+    String name;
+    String Address;
+    int Phone;
+    
+    
+}
