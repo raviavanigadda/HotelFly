6efc8094bd58ac692fe5ab7769ca3891237a0a3a
1894831 <1894831@E21011.CEGEPGIM.LCL>
2019-02-17 09:51:58 -0500

    Adding Hotel class with Constructor method. - Gulbeer

--- Hotelfly/src/hotelfly/Hotel.java
+++ Hotelfly/src/hotelfly/Hotel.java
@@ -10,9 +10,40 @@ package hotelfly;
  * @author 1894831
  */
 public class Hotel {
-    String name;
+    String Name;
     String Address;
     int Phone;
     
-    
+    public Hotel( String N,String A,int P)
+    {
+        this.Name=N;
+        this.Address=A;
+        this.Phone=P;    
+    }    
+    public void setName( String N)
+    {
+        this.Name=N;
+    }
+    public String getName ()
+    {
+    return(Name);
+    }
+    public void setAddress(String A)
+    {
+    this.Address=A;
+    }
+    public String getAddress()
+    {
+      return(Address);  
+    }
+    public void setPhone(int P)
+    {
+    this.Phone=P;
+    }
+    public int getPhone()        
+    {
+    return(Phone);
+    }
+
+
 }
