516655b2effb8b1441f3ff57b6f44e138fb039bc
1895126 <1895126@E21031.CEGEPGIM.LCL>
2019-03-02 16:40:21 -0500

    Commiting test files - Sukh

---
+++ Hotelfly/test/Detail.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 1895126
+ */
+public class Detail {
+    
+    public Detail() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+     public void detailTest(){
+     
+         int nop=4;
+         int id=5;
+         String exprslt=nop+" "+id;
+        String rslt=hotelfly.Detail.disp(nop, id);
+         assertEquals(exprslt, rslt);
+         
+     }
+}
